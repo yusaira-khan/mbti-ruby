928839ec43d8a199ac9eefb80db451120fda7acb
Yusaira Khan <yusaira.khan@mail.mcgill.ca>
2020-12-17 22:18:10 -0500

    add classes

---
+++ src/mb.rb
@@ -0,0 +1,24 @@
+class Trait
+  attr_reader :energy
+  attr_reader :mind
+  attr_reader :nature
+  attr_reader :tactics
+  attr_reader :percentage
+
+  def initialize
+  end
+end
+class Aspect
+
+  attr_reader :left
+  attr_reader :right
+  def initialize
+  end
+end
+class Preference
+  attr_reader :opposite
+  attr_reader :percent
+  def initialize
+
+  end
+end
