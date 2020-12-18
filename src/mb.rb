91b88fe88a40218e071ecd5dee8ec6eb6d42ceaa
Yusaira Khan <yusaira.khan@mail.mcgill.ca>
2020-12-17 22:30:38 -0500

    add pseudo code/ todos

--- src/mb.rb
+++ src/mb.rb
@@ -1,12 +1,25 @@
+class Personality
+  def initialize
+    # add all 16 personalities
+  end
+  def prindata
+    #sort
+  end
+end
 class Trait
   attr_reader :energy
   attr_reader :mind
   attr_reader :nature
   attr_reader :tactics
   attr_reader :percentage
+  attr_reader :role_name
+  attr_reader :initials
+  attr_reader :link
 
   def initialize
+    # load data
   end
+
 end
 class Aspect
 
@@ -14,11 +27,20 @@ class Aspect
   attr_reader :right
   def initialize
   end
+
+  def dominant
+    #get dominant between right and left
+  end
 end
 class Preference
   attr_reader :opposite
-  attr_reader :percent
-  def initialize
+  attr_accessor :percent
+  attr_reader :name
+  attr_reader :initials
 
+  def initialize
+  end
+
+  def print
   end
 end
