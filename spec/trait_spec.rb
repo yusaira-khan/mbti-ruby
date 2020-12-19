9e6d3286f06e43ce70716c2d88bc316b2a0ce50c
Yusaira Khan <yusaira.khan@mail.mcgill.ca>
2020-12-19 16:00:53 -0500

    add trait fraction test

--- spec/trait_spec.rb
+++ spec/trait_spec.rb
@@ -1,12 +1,12 @@
 require 'trait'
 require 'preference'
 
+MockPrefsC = Struct.new(:collection)
 RSpec.describe Trait do
   it 'construct from intials' do
     #arrange
     collection = {'e'=>:e, 'n'=>:n, 'f'=>:f, 'p'=>:p}
-    MockPref2 = Struct.new(:collection)
-    mock = MockPref2.new(collection)
+    mock = MockPrefsC.new(collection)
 
     #act
     t = Trait.new(initials: 'enfp', role_name: 'Me', prefs: mock)
@@ -20,4 +20,18 @@ RSpec.describe Trait do
     expect(t.tactics).to eq(:p)
     expect(t.link).to eq('https://www.16personalities.com/enfp-personality')
   end
+  it 'Fraction' do
+    #arrange
+    MockPrefFrac = Struct.new(:adjust_frac)
+    i = MockPrefFrac.new(0.9)
+    c = {'i': i}
+    mock = MockPrefsC.new(c)
+
+    #act
+    t = Trait.new(initials: 'iiii', role_name: '', prefs: mock)
+
+    #assert
+    expect(t.fraction).to eq(0.9**4)
+  end
+
 end
