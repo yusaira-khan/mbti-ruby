c70c2a8d396fe0d0a5c75455bc0844d63572e981
Yusaira Khan <yusaira.khan@mail.mcgill.ca>
2020-12-19 15:30:05 -0500

    make trait work with prefs list mock

--- spec/trait_spec.rb
+++ spec/trait_spec.rb
@@ -3,13 +3,21 @@ require 'preference'
 
 RSpec.describe Trait do
   it 'construct from intials' do
-    t = Trait.new(initials: 'enfp', role_name: 'Me')
+    #arrange
+    collection = {'e'=>:e, 'n'=>:n, 'f'=>:f, 'p'=>:p}
+    MockPref2 = Struct.new(:collection)
+    mock = MockPref2.new(collection)
+
+    #act
+    t = Trait.new(initials: 'enfp', role_name: 'Me', prefs: mock)
+
+    #assert
     expect(t.initials).to eq('enfp')
     expect(t.role_name).to eq('Me')
-    expect(t.mind).to eq('e')
-    expect(t.energy).to eq('n')
-    expect(t.nature).to eq('f')
-    expect(t.tactics).to eq('p')
+    expect(t.mind).to eq(:e)
+    expect(t.energy).to eq(:n)
+    expect(t.nature).to eq(:f)
+    expect(t.tactics).to eq(:p)
     expect(t.link).to eq('https://www.16personalities.com/enfp-personality')
   end
 end
--- src/trait.rb
+++ src/trait.rb
@@ -12,10 +12,11 @@ class Trait
   def link
     "https://www.16personalities.com/#{initials}-personality"
   end
-  def initialize(initials:, role_name:)
+  def initialize(initials:, role_name:, prefs:)
     @initials = initials
     @role_name = role_name
-    @mind, @energy, @nature, @tactics = @initials.split('')
+    @mind, @energy, @nature, @tactics = \
+                             @initials.split('').map{|i| prefs.collection[i]}
 
   end
 
