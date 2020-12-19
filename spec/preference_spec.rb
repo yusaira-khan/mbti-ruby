15d833ef9bd1309a2872214598747717b85668e3
Yusaira Khan <yusaira.khan@mail.mcgill.ca>
2020-12-18 19:03:38 -0500

    can successfully read csv

--- spec/preference_spec.rb
+++ spec/preference_spec.rb
@@ -1,5 +1,14 @@
+require 'preference'
+require 'csv'
 
-RSpec.describe 'preference test' do
-  it 'empty' do
+RSpec.describe PreferencesList do
+  it 'constructor empty' do
+    p = PreferencesList.new
+  end
+  it 'can read csv now' do
+    table = CSV.parse(File.read('./data/preference.csv'), headers: true)
+    expect(table[0]['name']).to eq('Introverted')
+    expect(table[0]['initial']).to eq('i')
+    expect(table[0]['aspect']).to eq('mind')
   end
 end
