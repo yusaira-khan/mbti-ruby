bda347f76b0645695aecfa60ed55472332e87514
Yusaira Khan <yusaira.khan@mail.mcgill.ca>
2020-12-20 19:22:52 -0500

    reformat traits

--- src/trait.rb
+++ src/trait.rb
@@ -1,4 +1,3 @@
-
 class TraitsList
 end
 class Trait
@@ -12,16 +11,15 @@ class Trait
   def link
     "https://www.16personalities.com/#{initials}-personality"
   end
+
   def initialize(initials:, role_name:, prefs:)
     @initials = initials
     @role_name = role_name
     @mind, @energy, @nature, @tactics = \
-                             @initials.split('').map{|i| prefs.collection[i]}
-
+      @initials.split('').map { |i| prefs.collection[i] }
   end
 
   def fraction
     @mind.adjust_frac * @nature.adjust_frac * @energy.adjust_frac * @tactics.adjust_frac
   end
-
 end
