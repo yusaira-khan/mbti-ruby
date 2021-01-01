d7d0d5983fcfd19311a668f0869d136d7bfe3314
Yusaira Khan <yusaira.khan@mail.mcgill.ca>
2021-01-01 16:39:38 -0500

    add get stuff and separate main

--- src/app.rb
+++ src/app.rb
@@ -1,4 +1,8 @@
 require 'sinatra'
 get '/' do
+  # matches "GET /posts?i=foo&author=bar"
+  title = params['title']
+    author = params['author']
   'Hello world!'
+
 end
--- src/main.rb
+++ src/main.rb
@@ -5,21 +5,6 @@ require 'trait'
 require 'optparse'
 require 'sanitizer'
 
-class Personality
-  def initialize
-    @prefs = PreferencesRepository.new
-    @aspects = AspectsRepository.new @prefs
-    @traits = TraitsRepository.new @prefs
-  end
-
-  def quick!(l)
-    @aspects.quick!(l)
-    display
-  end
-  def display()
-    @traits.sorted.map(&:display).join("\n")
-  end
-end
 
 
 per = Personality.new
